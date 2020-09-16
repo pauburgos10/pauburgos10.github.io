fc595a8670f7eeb987a2e9c1399cc4d036e80f99
Paula Burgos <paula.burgos@valtira.net>
2020-09-16 16:04:49 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarAlert() {
+  alert("Enviado!");
+}
+
+let noPodesEliminar = () => {
+  alert('Ahora mismo no podés eliminar este ítem')
+};
+
+let peliRecomendada = () => {
+  document.body.querySelector('#recomendada').style.background= 'magenta';
+}
